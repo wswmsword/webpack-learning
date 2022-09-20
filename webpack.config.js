050837e65f3f2f623ffeab131f2c5f05147b7bc5
wswmsword <cjzjzzz@gmail.com>
2022-09-20 14:37:34 +0800

    add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.(png|svg|jpeg|jpg|gif)$/i,
         type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       }
     ]
   }
