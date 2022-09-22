edc10ed81e4a71e9f2f252280a6d2700f48cd83f
wswmsword <cjzjzzz@gmail.com>
2022-09-23 07:03:09 +0800

    lazy load print.js after clicking

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-async function getComponent() {
+import _ from "lodash";
 
-  const { default: _ } = await import("lodash");
+function component() {
+
   const element = document.createElement("div");
+
+  const button = document.createElement("button");
+  const br = document.createElement("br");
+
+  button.innerHTML = "Click me and loot at the console!";
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
+
+  button.onclick = e => import(/* webpackChunkName: "print666" */ './print').then(module => {
+    const print = module.default;
+
+    print();
+  });
   return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
     index: "./src/index.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "懒加载",
+    }),
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   optimization: {
     runtimeChunk: "single", // [一个页面多个入口时共享一个实例](https://bundlers.tooling.report/code-splitting/multi-entry/)
