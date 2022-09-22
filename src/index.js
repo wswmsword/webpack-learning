1f556454e820b094a393026e3c59b4bca615c5a8
wswmsword <cjzjzzz@gmail.com>
2022-09-22 16:56:59 +0800

    tree-shaking

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import _ from "lodash";
+// import _ from "lodash";
+import { cube } from "./math.js";
 
 function component() {
-  const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack', ' ']);
+  // const element = document.createElement('div');
+  const element = document.createElement('pre');
+  // element.innerHTML = _.join(['Hello', 'webpack', ' ']);
+  const s = document.getElementsByTagName("body").length;
+  element.innerHTML = [
+    "Hello, webpack!",
+    "5 cubes is equal to " + cube(s)
+  ].join("\n\n");
 
   return element;
 }
