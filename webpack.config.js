2301e6fe0eac4354abce819218905cc874fdb930
wswmsword <cjzjzzz@gmail.com>
2022-09-22 16:33:59 +0800

    chapter gotchas

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
+    app: "./src/index.js",
   },
   devtool: "inline-source-map",
   devServer: {
     static: "./dist",
+    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Development",
+      title: "Hot Module Replacement",
     }),
   ],
   output: {
