744316872b0ea87f1d02728b088b70112353611c
wswmsword <cjzjzzz@gmail.com>
2022-09-23 08:51:16 +0800

    chapter shimming-globals

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import _ from "lodash";
-
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack', ' ']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "shimming",
+    }),
+    new webpack.ProvidePlugin({
+      _: "lodash",
+    }),
+  ],
 }
